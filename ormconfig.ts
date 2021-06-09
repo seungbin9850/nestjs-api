cc916f40c4279d77019cdc474bcedf0f04bd7702
seungbin9850 <tanja1206@naver.com>
2021-06-09 16:59:40 +0900

    [ADD] ormconfig

---
+++ ormconfig.ts
@@ -0,0 +1,17 @@
+import config from './src/config';
+import { ConnectionOptions } from 'typeorm';
+import { User } from './src/entities';
+
+const connectionOptions: ConnectionOptions = {
+  type: 'mysql',
+  host: config.mysql.host,
+  port: 3306,
+  username: config.mysql.username,
+  password: config.mysql.password,
+  database: config.mysql.name,
+  synchronize: true,
+  logging: true,
+  entities: [User],
+};
+
+export default connectionOptions;
