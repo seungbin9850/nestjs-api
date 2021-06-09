a7c76c6b441aa2e0a8765b8a0ebbbdc0fba6650e
seungbin9850 <tanja1206@naver.com>
2021-06-09 17:01:18 +0900

    [ADD] find one by id

---
+++ src/repositories/user.repository.ts
@@ -0,0 +1,13 @@
+import { User } from '../entities';
+import { EntityRepository, Repository } from 'typeorm';
+
+@EntityRepository(User)
+export class UserRepository extends Repository<User> {
+  async findOneById(userId: string) {
+    return await this.createQueryBuilder()
+      .where('user_id = :userId', {
+        userId,
+      })
+      .getOne();
+  }
+}
