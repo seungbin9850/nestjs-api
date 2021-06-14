597dacfb1761880619f96b6d8c639632a2993dcd
seungbin9850 <tanja1206@naver.com>
2021-06-14 14:50:55 +0900

    [ADD] create user service

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { HttpError } from 'src/exception';
+import { UserRepository } from 'src/repositories';
+import { hash } from 'src/utils';
+import { CreateUserDTO } from './dto/create-user.dto';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(private readonly userRepository: UserRepository) {}
+
+  async createUser(req: CreateUserDTO) {
+    const { userId, password } = req;
+    if (await this.findUser(userId))
+      throw new HttpError(409, 'Already Exists User');
+    const hashed = hash(password);
+    await this.userRepository.create({ userId, password: hashed });
+  }
+
+  private async findUser(userId: string) {
+    const user = await this.userRepository.findOneById(userId);
+    if (user) return true;
+    return false;
+  }
+}
