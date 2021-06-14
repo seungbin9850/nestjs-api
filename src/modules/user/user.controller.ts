1a75af433d56ae64841fa872291d08c84cab2851
seungbin9850 <tanja1206@naver.com>
2021-06-14 14:51:00 +0900

    [ADD] create user

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateUserDTO } from './dto/create-user.dto';
+import { UserService } from './user.service';
 
-@Controller('user')
-export class UserController {}
+@Controller('users')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Post('/')
+  async createUser(@Body() req: CreateUserDTO) {
+    await this.userService.createUser(req);
+    return { status: 201, message: 'success' };
+  }
+}
