68502bdbfb28c06acf4f7c271a1ad037fb7efc84
seungbin9850 <tanja1206@naver.com>
2021-06-14 14:49:59 +0900

    [ADD] use catch exception

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
+import { APP_FILTER } from '@nestjs/core';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import connectionOptions from 'ormconfig';
+import { CatchException } from './exception';
 import { UserModule } from './modules';
 
 @Module({
   imports: [TypeOrmModule.forRoot(connectionOptions), UserModule],
-  providers: [],
+  providers: [{ provide: APP_FILTER, useClass: CatchException }],
 })
 export class AppModule {}
