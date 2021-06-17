c4c7d453b8ebe2be187a964539805f0e1b627778
seungbin9850 <tanja1206@naver.com>
2021-06-17 12:07:20 +0900

    [FIX] user id primary column

--- src/entities/user.entity.ts
+++ src/entities/user.entity.ts
@@ -2,10 +2,7 @@ import { Column, Entity, PrimaryColumn } from 'typeorm';
 
 @Entity()
 export class User {
-  @PrimaryColumn('uuid', { length: 50 })
-  id: string;
-
-  @Column({ name: 'user_id', length: 12, unique: true })
+  @PrimaryColumn({ name: 'user_id', length: 12, unique: true })
   userId: string;
 
   @Column({ length: 80 })
