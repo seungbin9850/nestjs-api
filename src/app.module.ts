38e6c1ed355b655de925e3178ed7fe5d0fc194cb
seungbin9850 <tanja1206@naver.com>
2021-06-18 13:56:30 +0900

    [ADD] import auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,10 @@ import { APP_FILTER } from '@nestjs/core';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import connectionOptions from 'ormconfig';
 import { CatchException } from './exception';
-import { UserModule } from './modules';
+import { UserModule, AuthModule } from './modules';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(connectionOptions), UserModule],
+  imports: [TypeOrmModule.forRoot(connectionOptions), UserModule, AuthModule],
   providers: [{ provide: APP_FILTER, useClass: CatchException }],
 })
 export class AppModule {}
