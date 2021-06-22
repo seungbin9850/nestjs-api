0660c00aeca0f825319479d75f51b15d660fc676
seungbin9850 <tanja1206@naver.com>
2021-06-23 08:55:33 +0900

    [FIX] primary key uuid

--- src/entities/user.entity.ts
+++ src/entities/user.entity.ts
@@ -1,10 +1,17 @@
-import { Column, Entity, PrimaryColumn } from 'typeorm';
+import { Column, Entity, OneToMany, PrimaryColumn } from 'typeorm';
+import { Post } from './post.entity';
 
 @Entity()
 export class User {
-  @PrimaryColumn({ name: 'user_id', length: 12, unique: true })
+  @PrimaryColumn({ length: 45 })
+  id: string;
+
+  @Column({ name: 'user_id', length: 12, unique: true })
   userId: string;
 
   @Column({ length: 80 })
   password: string;
+
+  @OneToMany((type) => Post, (post) => post.user)
+  posts!: Post[];
 }
