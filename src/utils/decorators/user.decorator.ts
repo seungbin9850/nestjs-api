2d97a082812df36bc948b2324703856c80be7cd3
seungbin9850 <tanja1206@naver.com>
2021-06-23 08:57:03 +0900

    [ADD] token decorator

---
+++ src/utils/decorators/user.decorator.ts
@@ -0,0 +1,8 @@
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+
+export const Token = createParamDecorator(
+  (data, ctx: ExecutionContext): ParameterDecorator => {
+    const request = ctx.switchToHttp().getRequest();
+    return request.decoded;
+  },
+);
