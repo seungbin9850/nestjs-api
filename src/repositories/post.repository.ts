dc69879ea575e6921789158bdc9973ed52a2e62c
seungbin9850 <tanja1206@naver.com>
2021-06-29 17:16:17 +0900

    [ADD] get posts list

--- src/repositories/post.repository.ts
+++ src/repositories/post.repository.ts
@@ -2,4 +2,11 @@ import { Post } from 'src/entities/post.entity';
 import { EntityRepository, Repository } from 'typeorm';
 
 @EntityRepository(Post)
-export class PostRepository extends Repository<Post> {}
+export class PostRepository extends Repository<Post> {
+  async getList(page: number) {
+    return await this.createQueryBuilder('post')
+      .offset(page * 3)
+      .limit(3)
+      .getMany();
+  }
+}
