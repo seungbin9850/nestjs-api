a53daf0971da9ec3588977a79ea6e883c40d217a
seungbin9850 <tanja1206@naver.com>
2021-06-29 17:16:36 +0900

    [ADD] get posts list controller

--- src/modules/post/post.controller.ts
+++ src/modules/post/post.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query, UseGuards } from '@nestjs/common';
 import { AuthGuard } from 'src/middlewares';
 import { Token } from 'src/utils/decorators/user.decorator';
 import { WritePostDTO } from './dto';
+import { GetPostsListDTO } from './dto/get-posts-list.dto';
 import { PostService } from './post.service';
 
 @Controller('posts')
@@ -13,5 +14,23 @@ export class PostController {
   async writePost(@Body() req: WritePostDTO, @Token() decoded: any) {
     await this.postService.create(req, decoded.id);
     return { status: 201, message: 'success' };
+  }
+
+  @Get('/')
+  @UseGuards(new AuthGuard())
+  async getPosts(@Query() req: GetPostsListDTO, @Token() decoded: any) {
+    const { id } = decoded;
+    const posts = await this.postService.getPostList(req);
+    const response = posts.map((post) => {
+      post['isMine'] = false;
+      if (post.userId === id) post['isMine'] = true;
+      return {
+        id: post.id,
+        title: post.title,
+        content: post.content,
+      };
+    });
+
+    return { status: 200, message: 'success', data: response };
   }
 }
