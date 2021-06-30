9373e7a39ee9a191873ccfde4b831ccf843685fd
seungbin9850 <tanja1206@naver.com>
2021-06-30 09:24:01 +0900

    [ADD] update post service

--- src/modules/post/post.service.ts
+++ src/modules/post/post.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@nestjs/common';
 import { Post } from 'src/entities/post.entity';
+import { HttpError } from 'src/exception';
 import { PostRepository } from 'src/repositories';
 import { makeId } from 'src/utils/uuid';
-import { WritePostDTO } from './dto';
+import { UpdatePostDTO, WritePostDTO } from './dto';
 import { GetPostsListDTO } from './dto/get-posts-list.dto';
 
 @Injectable()
@@ -22,5 +23,14 @@ export class PostService {
   async getPostList(req: GetPostsListDTO) {
     const { page } = req;
     return await this.postRepository.getList(Number(page));
+  }
+
+  async updatePost(req: UpdatePostDTO, userId: string, postId: string) {
+    const { title, content } = req;
+    const post = await this.postRepository.findOne(postId);
+    if (post.userId !== userId) throw new HttpError(403, 'Not Your Post');
+    post.title = title;
+    post.content = content;
+    await this.postRepository.save(post);
   }
 }
