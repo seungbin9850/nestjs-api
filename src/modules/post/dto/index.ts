194f5ac4e6a4a879d54158aec6d3c1e79ac83349
seungbin9850 <tanja1206@naver.com>
2021-07-01 19:11:06 +0900

    [ADD] export

--- src/modules/post/dto/index.ts
+++ src/modules/post/dto/index.ts
@@ -2,3 +2,4 @@ export * from './write-post.dto';
 export * from './get-posts-list.dto';
 export * from './update-post.dto';
 export * from './get-detail-post.dto';
+export * from './delete-post.dto';
